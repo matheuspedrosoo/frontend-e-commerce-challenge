c85d583a750408516b2d179fba3a2fa83e6052a4
Matheus Pedroso <matheuspedroso404@gmail.com>
2023-05-25 16:16:35 -0300

    fix: error undefined localStorage

--- capputeeno/src/components/Header/cart-control.tsx
+++ capputeeno/src/components/Header/cart-control.tsx
@@ -20,12 +20,12 @@ const CartCount = styled.span`
 `
 
 export function CartControl() {
-	const { value } = useLocalStorage('cart-items')
+	const { value } = useLocalStorage('cart-items', [])
 
 	return (
 		<Container>
 			<CartIcon />
-			{value.length > 0 && <CartCount>2</CartCount>}
+			{value.length > 0 && <CartCount>{value.length}</CartCount>}
 		</Container>
 	)
 }
--- capputeeno/src/hooks/useLocalStorage.tsx
+++ capputeeno/src/hooks/useLocalStorage.tsx
@@ -1,10 +1,14 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-export function useLocalStorage<T>(item: string) {
-	const storedValue = localStorage.getItem(item)
-	const parsedValue = storedValue ? JSON.parse(storedValue) : ''
-	const [value, setValue] = useState(parsedValue)
+export function useLocalStorage<T>(item: string, initialValue: T) {
+	const [value, setValue] = useState<T>(initialValue)
+
+	useEffect(() => {
+		if (typeof window === 'undefined') return
+		let value = localStorage.getItem(item)
+		if (value) setValue(JSON.parse(value))
+	}, [item])
 
 	const updateLocalStorage = (newValue: T) => {
 		setValue(newValue)
